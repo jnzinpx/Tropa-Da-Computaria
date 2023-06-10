9f53182c3bf90a9fe975d0f4fee5b924a581edb8
jnzinpx <jhonataaltamir9@gmail.com>
2023-06-10 15:51:43 -0300

    mudaças all

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ window.addEventListener('resize', function() {
         container.classList.remove('small', 'medium');
     }
 });
+
+window.onload = function() {
+    const container = document.querySelector('.container2');
+    const numBubbles = 10; // Define o número de bolhas que você deseja
+  
+    for (let i = 0; i < numBubbles; i++) {
+      const bubble = document.createElement('div');
+      bubble.classList.add('bubble');
+      bubble.style.left = Math.random() * container.offsetWidth + 'px';
+      bubble.style.top = Math.random() * container.offsetHeight + 'px';
+      bubble.style.animationDuration = Math.random() * 6 + 4 + 's'; // Define a duração da animação de cada bolha
+      container.appendChild(bubble);
+    }
+  }
+  
+  
+  
